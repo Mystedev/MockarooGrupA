f1707dd1e5156b593dfc7962894ebd5b7ae0af4a
Mystedev <moya-deny14@hotmail.com>
2024-09-25 20:10:42 +0200

    Funcion de Numero Aleatorio

--- src/Mockaroo.java
+++ src/Mockaroo.java
@@ -1,12 +1,15 @@
 import java.io.FileReader;
+import java.util.Random;
 import java.io.File;
 import java.io.BufferedReader;
+import java.util.Scanner;
 
 public class Mockaroo {
 
 	public static void main(String[] args) {
 		
 		try {
+			Scanner reader = new Scanner(System.in);
 			String files_dades[]= {"Dades/1-Noms.txt","Dades/2-Cognoms.txt","Dades/3-Ciutat.txt","Dades/4-Adreces.txt","Dades/5.Proffesions.txt",
 					"Dades/6.Pais.txt","Dades/7.Estudis.txt","Dades/8.Colors.txt","Dades/10.NomDeLaCompanyia"};
 			FileReader fr = new FileReader(files_dades[0]);
@@ -16,6 +19,7 @@ public class Mockaroo {
 				line = br.readLine();
 				System.out.println(line);
 			}
+			// System.out.println(RandomNumber(1,0,1000));
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -23,6 +27,18 @@ public class Mockaroo {
 	// ·Leemos archivos con funciones y generamos estos a partir de otras funciones ->
 	// ·Funcion boolean sin formatos
 	// ·Funcion int para indicar los 'decimales=0','minims=0 i maxims=1000'
+	public static double RandomNumber(double decimals,double minim,double maxim) {
+		// Importem el metode random per generar numeros aleatoris
+		Random random = new Random();
+		// Generem el numero aleatori entre un (minim i un maxim)
+        double numeroAleatorio = minim + (maxim - minim) * random.nextDouble(); 
+        // Redondearem el numeor especificat a partir de la quantitat de decimals demanats
+        double escala = Math.pow(10, decimals);
+        numeroAleatorio = Math.round(numeroAleatorio * escala) / escala;
+		// Al declarar el resultat de la funcio , haurem de especificar en ordre , la quantitat de decimals que volem, el rang minim i el rang maxim
+        // Decimals -> 0 per defecte * Minim -> 0 per defecte * Maxim -> 1000 per defecte
+		return numeroAleatorio;
+	}
 	// ·Funcion String para indicar el nombre del dominio='nom de comapnyia'
 	// ·Funcion IP4 ???
 	// ·Funcion String ha de indicar 'letras,numeros,mayusculas,minusculas,simbolos y longitud'
