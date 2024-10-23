8f588e54316ab77d03ab3a64bdc3d888fe1556ea
Jaume <Jaume@DESKTOP-SI9OIUV>
2024-10-23 20:49:32 +0200

    eliminar comentarios basura

--- src/Mockaroo.java
+++ src/Mockaroo.java
@@ -599,16 +599,7 @@ public class Mockaroo {
   		}
   	}
   	
-	// ·Leemos archivos con funciones y generamos estos a partir de otras funciones ->
-	// ·Funcion boolean sin formatos
-	// ·Funcion int para indicar los 'decimales=0','minims=0 i maxims=1000'
-	// ·Funcion String para indicar el nombre del dominio='nom de comapnyia'
-	// ·Funcion IP4 ???
-	// ·Funcion String ha de indicar 'letras,numeros,mayusculas,minusculas,simbolos y longitud'
-	// ·Funcion String ha de indicar el 'any minim=1900 i maxim=2023'
-	// ·Funcion para IBAN o DNI (Hay como buscar como se generan)
-	// ·Funcion int ha de indicar el 'valor d'inici=1'
-	//*****************************
+	
 	// Debemos leer el archivo de datos y crear a partir de este los archivos SQL y XML/XSD/XSLT
 	public static void creacioDeSql(String ruta,int linies,String [][] dadesCrear2 ) throws IOException {
 		
